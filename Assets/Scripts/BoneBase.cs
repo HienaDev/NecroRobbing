769707c7b563745252d5d153938c0c2048d5a1fe
Julia Costa <juliacosta232005@gmail.com>
2024-10-18 22:50:59 +0100

    Create Bone1 Example and Add Sprite Parameter to BoneBase

--- Assets/Scripts/BoneBase.cs
+++ Assets/Scripts/BoneBase.cs
@@ -3,7 +3,11 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "BoneBase", menuName = "Scriptable Objects/BoneBase")]
 public class Bone : ScriptableObject
 {
-    private BoneType _boneType;
-    private float _boneSize;
-    private float _powerLevel;
+    [SerializeField] private BoneType _boneType;
+    public BoneType BoneType => _boneType;
+    [SerializeField] private float _boneSize;
+    public float BoneSize => _boneSize;
+    [SerializeField] private float _powerLevel;
+    public float PowerLevel => _powerLevel;
+    [SerializeField] private Sprite _boneSprite;
 }
