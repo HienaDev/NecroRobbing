4d39ce96c520eb3d511e4cd1f4e36d0162e3ef46
Julia Costa <juliacosta232005@gmail.com>
2024-10-19 21:09:05 +0100

    Add InventoryManager

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -1,16 +1,31 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class InventoryManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private List<BoneBase> skullInventory;
+    private List<BoneBase> legInventory;
+    private List<BoneBase> toraxInventory;
+    private List<BoneBase> armInventory;
+    public void AddInventory(List<BoneBase> inventory)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        foreach (BoneBase bone in inventory)
+        {
+            switch (bone.BoneType)
+            {
+                case BoneType.Head:
+                    skullInventory.Add(bone);
+                    break;
+                case BoneType.Arm:
+                    armInventory.Add(bone);
+                    break;
+                case BoneType.Torso:
+                    toraxInventory.Add(bone);
+                    break;
+                case BoneType.Leg:
+                    legInventory.Add(bone);
+                    break;
+            }
+        }   
     }
 }
