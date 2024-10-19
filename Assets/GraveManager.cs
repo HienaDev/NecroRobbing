60dbef0933806e8da95170564d9c163bf140df40
HienaDev <ant.rodrigues1999@gmail.com>
2024-10-19 19:57:06 +0100

    Add grave selection

--- Assets/GraveManager.cs
+++ Assets/GraveManager.cs
@@ -1,23 +1,44 @@
+using NUnit.Framework;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class GraveManager : MonoBehaviour
 {
+    [SerializeField] private GameObject graveSelection;
     [SerializeField] private GameObject graveDigging;
     private CheckIfPlayerInside checkPlayerScript;
+
+    private SpriteRenderer[] renderers;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         checkPlayerScript = GetComponentInChildren<CheckIfPlayerInside>();
+        renderers = GetComponentsInChildren<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (checkPlayerScript.PlayerInside && Input.GetKeyDown(KeyCode.E))
+        if (checkPlayerScript.PlayerInside)
         {
-            graveDigging.SetActive(true);
-            GetComponentInChildren<SpriteRenderer>().color = Color.red;
+            foreach (SpriteRenderer sr in renderers)
+            {
+                sr.color = Color.red;
+            }
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                graveSelection.SetActive(false);
+                graveDigging.SetActive(true);
+            }
+                
+        }
+        else
+        {
+            foreach (SpriteRenderer sr in renderers)
+            {
+                sr.color = Color.white;
+            }
         }
     }
 
